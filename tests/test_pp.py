671e7a802e1a71d02d4227641fa37b42626f4361
Bernhard Eder <bernheder01@gmail.com>
2023-04-01 17:22:57 +0200

    pseudo bulk method

--- tests/test_pp.py
+++ tests/test_pp.py
@@ -1,14 +1,20 @@
+from pathlib import Path
+
 import anndata as ad
 import numpy as np
 import pandas as pd
 
 import rectangle
 
+TEST_DATA_DIR = Path(__file__).resolve().parent / "data"
+
 
 def test_pseudo_bulk_creation_anndata():
     group_size = 3
-    sc_data = pd.read_csv("./data/sc_object_small.csv", index_col=0)
-    annotations = np.array(list(pd.read_csv("./data/cell_annotations_small.txt", header=None, index_col=0).index))
+    sc_data = pd.read_csv(TEST_DATA_DIR / "sc_object_small.csv", index_col=0)
+    annotations = np.array(
+        list(pd.read_csv(TEST_DATA_DIR / "cell_annotations_small.txt", header=None, index_col=0).index)
+    )
     annotations = pd.DataFrame(annotations, index=sc_data.columns)
     adata = ad.AnnData(X=sc_data.values.T, obs=annotations, var=pd.DataFrame(data=sc_data.index, index=sc_data.index))
     result = rectangle.pp.make_pseudo_bulk_adata(adata, group_size)
