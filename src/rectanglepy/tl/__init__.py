0701c30b4072bf90f96ee0576bd435ed89dc41f7
Bernhard Eder <bernheder01@gmail.com>
2023-04-18 13:33:54 +0200

    export dconvolution functions

--- src/rectanglepy/tl/__init__.py
+++ src/rectanglepy/tl/__init__.py
@@ -1,6 +1,7 @@
 from .basic import (
     correct_for_unknown_cell_content,
     create_cpm_pseudo_signature,
+    direct_deconvolute,
     recursive_deconvolute,
     scale_weights,
     solve_dampened_wsl,
