f352f63f1d685a704008936a5883883e3c8e30d1
bernheder <bernheder01@gmail.com>
2023-05-13 16:19:42 +0200

    documentation

--- docs/conf.py
+++ docs/conf.py
@@ -87,6 +87,7 @@ source_suffix = {
 }
 
 intersphinx_mapping = {
+    "pandas": ("https://pandas.pydata.org/docs/", None),
     "python": ("https://docs.python.org/3", None),
     "anndata": ("https://anndata.readthedocs.io/en/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
