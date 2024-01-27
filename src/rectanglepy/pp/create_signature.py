2bc2ef6477c0fe6cb316d88417cf8bb92f555c36
bernheder <bernheder01@gmail.com>
2024-01-27 15:13:47 +0100

    stricter cutoffs

--- src/rectanglepy/pp/create_signature.py
+++ src/rectanglepy/pp/create_signature.py
@@ -343,7 +343,7 @@ def _optimize_parameters(
     # if there are many cell types we relax the cutoffs
     if len(pseudo_signature_counts.columns) > 8:
         lfcs = [x / 100 for x in range(140, 200, 10)]
-        ps = [x / 1000 for x in range(15, 20, 1)]
+        ps = [x / 1000 for x in range(13, 18, 1)]
 
     if len(pseudo_signature_counts.columns) > 15:
         lfcs = [x / 100 for x in range(60, 110, 10)]
